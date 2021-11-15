f756edff91a5152a99ff30545bbc3e95ef53d125
ypc <2412038772@qq.com>
2021-11-15 09:28:57 +0800

    update(sourceVector):  数据源触发事件

--- src/components/sources/SourceVector.vue
+++ src/components/sources/SourceVector.vue
@@ -1,113 +1,118 @@
 <template>
-<div>
+  <div>
     <slot></slot>
-</div>
+  </div>
 </template>
 
 <script>
 import VectorSource from 'ol/source/Vector';
 
 import {
-    inject,
-    watch,
-    onMounted,
-    onUnmounted,
-    provide,
-    computed
+  inject,
+  watch,
+  onMounted,
+  onUnmounted,
+  provide,
+  computed
 } from 'vue'
 
 import usePropsAsObjectProperties from '@/composables/usePropsAsObjectProperties'
-
+// changefeature //https://openlayers.org/en/latest/apidoc/module-ol_source_Vector.VectorSourceEvent.html#event:changefeature
 export default {
-    name: 'ol-source-vector',
-    setup(props) {
+  name: 'ol-source-vector',
+  emits: ['change'],
+  setup(props, { emit }) {
 
-        const layer = inject('vectorLayer');
+    const layer = inject('vectorLayer');
 
-        const {
-            properties
-        } = usePropsAsObjectProperties(props);
+    const {
+      properties
+    } = usePropsAsObjectProperties(props);
 
-        let source = computed(() => new VectorSource(properties));
+    let source = computed(() => new VectorSource(properties));
 
-        const applySource = () => {
-            layer.value.setSource(null)
-            layer.value.setSource(source.value)
-            layer.value.changed()
-        };
-        watch(properties, () => {
-            applySource();
+    const applySource = () => {
+      layer.value.setSource(null)
+      layer.value.setSource(source.value)
+      layer.value.changed()
+    };
 
-        })
+    source.value.on('change', () => {
+      emit('change', source.value.getFeatures())
+    })
 
-        watch(layer, () => {
-            applySource();
-        });
+    watch(properties, () => {
+      applySource();
 
-        onMounted(() => {
-            layer.value.setSource(source.value)
-        });
+    })
 
-        onUnmounted(() => {
-            layer.value.setSource(null)
-        });
+    watch(layer, () => {
+      applySource();
+    });
 
-        provide("vectorSource", source);
+    onMounted(() => {
+      layer.value.setSource(source.value)
+    });
 
-        return {
-            layer,
-            source
-        }
+    onUnmounted(() => {
+      layer.value.setSource(null)
+    });
+
+    provide("vectorSource", source);
+
+    return {
+      layer,
+      source
+    }
+  },
+  props: {
+    attributions: {
+      type: [String, Array],
     },
-    props: {
-        attributions: {
-            type: [String, Array],
-        },
-        features: {
-            type: Array,
-            default: () => []
-        },
-        format: {
-            type: Object
+    features: {
+      type: Array,
+      default: () => []
+    },
+    format: {
+      type: Object
 
-        },
-        loader: {
-            type: Function
+    },
+    loader: {
+      type: Function
 
-        },
-        overlaps: {
-            type: Boolean,
-            default: true
+    },
+    overlaps: {
+      type: Boolean,
+      default: true
 
-        },
-        projection: {
-            type: String,
-            default: 'EPSG:3857'
-        },
-        strategy: {
-            type: Function
+    },
+    projection: {
+      type: String,
+      default: 'EPSG:3857'
+    },
+    strategy: {
+      type: Function
 
-        },
-        url: {
-            type: [String, Function]
+    },
+    url: {
+      type: [String, Function]
 
-        },
-        useSpatialIndex: {
-            type: Boolean,
-            default: true
+    },
+    useSpatialIndex: {
+      type: Boolean,
+      default: true
 
-        },
-        wrapX: {
-            type: Boolean,
-            default: true
-
-        }
+    },
+    wrapX: {
+      type: Boolean,
+      default: true
 
     }
+
+  }
 
 }
 </script>
 
 <style lang="">
-
 </style>
