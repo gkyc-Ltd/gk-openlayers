9d0b450edccc48c750f4108a6f96efdba1902641
ypc <2412038772@qq.com>
2021-11-18 17:23:31 +0800

    build: 发版1.5.9

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-import OpenLayersMap from '.'
+// import OpenLayersMap from ".";
 
+import OpenLayersMap from "gk-openlayers";
+import "gk-openlayers/dist/gk-openlayers.css";
 
+console.log("------------------------------------");
+console.log(OpenLayersMap);
+console.log("------------------------------------");
 const app = createApp(App);
-app.use(OpenLayersMap)
+app.use(OpenLayersMap);
 
-app.mount('#app')
+app.mount("#app");
