ba5f24fb4920b83d27765f8b0ed93a7a6132ef56
ypc <2412038772@qq.com>
2021-11-29 15:21:32 +0800

    feat(mousePosition): 格式化+样式

--- src/components/mapControls/MousePositionControl.vue
+++ src/components/mapControls/MousePositionControl.vue
@@ -7,45 +7,46 @@
 <script>
 
 import {
-    MousePosition
+  MousePosition
 } from 'ol/control';
 import useControl from '@/composables/useControl'
+import { createStringXY } from 'ol/coordinate';
 
 export default {
-    name: 'ol-mouseposition-control',
-    setup(props,context) {
-        const {
-            control
-        } = useControl(MousePosition, props,context);
-        return {
-            control
-        }
-    },
-    props: {
-        className: {
-            type: String,
-            default: 'ol-mouse-position'
-        },
-        coordinateFormat: {
-            type: String,
-        },
-        projection: {
-            type: String,
-        },
-        render: {
-            type: Function,
-        },
-        target: {
-            type: HTMLElement,
-        },
-        undefinedHTML: {
-            type: String,
-            default: '&#160;'
-        }
+  name: 'ol-mouseposition-control',
+  setup(props, context) {
+    const {
+      control
+    } = useControl(MousePosition, props, context);
+    return {
+      control
     }
+  },
+  props: {
+    className: {
+      type: String,
+      default: 'ol-mouse-position'
+    },
+    coordinateFormat: {
+      type: Function,
+      default: createStringXY(0)
+    },
+    projection: {
+      type: String,
+    },
+    render: {
+      type: Function,
+    },
+    target: {
+      type: HTMLElement,
+    },
+    undefinedHTML: {
+      type: String,
+      default: '&#160;'
+    }
+  }
 }
 </script>
 
 <style lang="">
-
 </style>
